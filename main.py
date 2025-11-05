82bfd316de16f94623bbffd59f5845e3ef07828a
Brett Fowler <b16fowler@gmail.com>
2025-11-05 11:47:56 -0500

    New get request

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://www.amctheatres.com/movie-theatres/plainville/amc-southington-12/showtimes")
-html = BeautifulSoup(response.text, 'html.parser')
-title = html.find('title').text
+def main():
+    '''
+    BeautifulSoup notes:
 
-# print(html.prettify())
-# print(html.find_all('a'))
-# print(html.get_text())
+    title = html.find("title").text
 
-for tag in html.find_all('a'):
-    print(tag.get_text())
+    print(html.prettify())
+    print(html.find_all("a"))
+    print(html.get_text())
+    '''
 
+    # Get request to the AMC Southington showtimes, then parse html
+    response_south = requests.get("https://www.amctheatres.com/movie-theatres/plainville/amc-southington-12/showtimes")
+    html_south = BeautifulSoup(response_south.text, "html.parser")
+    
+    # Finds and prints inner text for all 'a' tags for Southington theater
+    for tag in html_south.find_all("a"):
+        print(tag.get_text())
+
+    # Get request to the AMC Plainville showtimes, then parse html
+    response_plain = requests.get("https://www.amctheatres.com/movie-theatres/plainville/amc-plainville-20/showtimes")
+    html_plain = BeautifulSoup(response_plain.text, "htmlparser")
+
+    # Finds and prints inner text for all 'a' tags for Plainville theater
+    for tag in html_plain.find_all("a"):
+        print(tag.get_text())
+        
+
+if __name__ == "__main__":
+    main()
